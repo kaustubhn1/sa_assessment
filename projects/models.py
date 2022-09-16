1c2b9cf8701adc95207ef9378ae86bf5d66fdcc1
kaustubhn1 <narkhedekaustubh.think@gmail.com>
2022-09-16 20:39:00 +0530

    created apis for the CRUD operations

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+class Permissions(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+class Users(models.Model):
+    access = models.ManyToManyField(Permissions)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
     state_choices = (("Propose", "Propose"), ("Open", "Open"), ("Closed", "Closed"))
     name = models.CharField(max_length=100)
@@ -13,11 +27,12 @@ class Project(models.Model):
 
 class Access(models.Model):
     permit_choices = (("Read", "Read"), ("Create", "Create"), ("Update", "Update"), ("Delete", "Delete"))
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='project')
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='user', null=True)
     permit = models.CharField(max_length=50, choices=permit_choices)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['project', 'user_id', "permit"], name='unique')
+            models.UniqueConstraint(fields=['project', 'user', "permit"], name='unique')
         ]
+
