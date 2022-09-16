fba8b9fca81f257abe413877cbcb0851d679a56c
kaustubhn1 <narkhedekaustubh.think@gmail.com>
2022-09-16 12:51:41 +0530

    created database models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    state_choices = (("Propose", "Propose"), ("Open", "Open"), ("Closed", "Closed"))
+    name = models.CharField(max_length=100)
+    state = models.CharField(max_length=50, choices=state_choices)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Access(models.Model):
+    permit_choices = (("Read", "Read"), ("Create", "Create"), ("Update", "Update"), ("Delete", "Delete"))
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
+    permit = models.CharField(max_length=50, choices=permit_choices)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['project', 'user_id', "permit"], name='unique')
+        ]
